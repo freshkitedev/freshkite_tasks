c13575e066f3e004bd325c1e2e2fd24849a442f9
Freshkite <compsciuniv@gmail.com>
2024-12-15 11:45:19 +0530

    Add Todo react client base code

---
+++ projects/todo/client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+      <h1 className="flex justify-center items-center m-2 p-3 h-24 w-1/2 font-bold 
+      text-3xl text-center text-white hover:text-red-500
+      bg-red-500 sm:bg-green-500 md:bg-yellow-500 lg:bg-gray-500">
+        Bala is great
+      </h1>
+    </div>
+  )
+}
+
+export default App
