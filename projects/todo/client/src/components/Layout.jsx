f812474f31e49cbce8425cfd33e3b763280b91c1
Freshkite <compsciuniv@gmail.com>
2025-01-10 09:34:10 +0530

    Move Navbar to approute

--- projects/todo/client/src/components/Layout.jsx
+++ projects/todo/client/src/components/Layout.jsx
@@ -5,10 +5,6 @@ import { Outlet } from 'react-router-dom'
 const Layout = () => {
   return (
     <div>
-      <Navbar />
-      <main className="container mx-auto px-4 py-8">
-        <Outlet />
-      </main>
     </div>
   )
 }
--- projects/todo/client/src/routes/Approutes.jsx
+++ projects/todo/client/src/routes/Approutes.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Layout from '../components/Layout'
+import Navbar from '../components/Navbar'
 
 const Approutes = () => {
   return (
       <div>
+          <Navbar />
           <Routes>
               <Route path="/" element={<Layout />} />
           </Routes>
